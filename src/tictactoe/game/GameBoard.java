6d784e942e55170513bba7029857dc5a4b5d7907
thanhbrandon <thanhbrandon@tamu.edu>
2022-06-20 02:40:02 -0500

    Adds Icons to buttons

--- src/tictactoe/game/GameBoard.java
+++ src/tictactoe/game/GameBoard.java
@@ -27,7 +27,8 @@ public GameBoard()
 {
 // Set the title bar text.
 setTitle("Grid Layout");
-
+ImageIcon xIcon = new ImageIcon(new ImageIcon("x.jpg").getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
+ImageIcon oIcon = new ImageIcon(new ImageIcon("o.jpg").getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT));
 
 // Set the size of the window.
 setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -42,13 +43,13 @@ setLayout(new GridLayout(3, 5));
 JButton button1 = new JButton("");
 JButton button2 = new JButton("");
 JButton button3 = new JButton("");
-JLabel label4 = new JLabel("Player X");
-JLabel label5 = new JLabel("Score");
+JLabel label4 = new JLabel("Player X", JLabel.CENTER);
+JLabel label5 = new JLabel("Score", JLabel.CENTER);
 JButton button6 = new JButton("");
 JButton button7 = new JButton("");
 JButton button8 = new JButton("");
-JLabel label9 = new JLabel("Player O");
-JLabel label10 = new JLabel("Score");
+JLabel label9 = new JLabel("Player O", JLabel.CENTER);
+JLabel label10 = new JLabel("Score", JLabel.CENTER);
 JButton button11 = new JButton("");
 JButton button12 = new JButton("");
 JButton button13 = new JButton("");
@@ -68,7 +69,8 @@ button1.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button1.setEnabled(false); // Makes the button unpressable when pressed once
             char mark = game.getMark(); // Gets the player mark
-            button1.setText(mark + ""); // Marks the button with an X or O
+            //button1.setText(mark + ""); // Marks the button with an X or O
+            setMark(button1,mark,  xIcon,  oIcon);
             game.placeMark(0,0); // Places the mark into the array that checks if there is a winner
             if (game.checkForWin() == true){ // Checks if there is a winner if there is the board is reset
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -79,7 +81,7 @@ button2.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button2.setEnabled(false);
             char mark = game.getMark();
-            button2.setText(mark + "");
+            setMark(button2,mark,  xIcon,  oIcon);
             game.placeMark(0,1);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -90,7 +92,7 @@ button3.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button3.setEnabled(false);
             char mark = game.getMark();
-            button3.setText(mark + "");
+            setMark(button3,mark,  xIcon,  oIcon);
             game.placeMark(0,2);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -101,7 +103,7 @@ button6.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button6.setEnabled(false);
             char mark = game.getMark();
-            button6.setText(mark + "");
+            setMark(button6,mark,  xIcon,  oIcon);
             game.placeMark(1,0);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -112,7 +114,7 @@ button7.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button7.setEnabled(false);
             char mark = game.getMark();
-            button7.setText(mark + "");
+            setMark(button7,mark,  xIcon,  oIcon);
             game.placeMark(1,1);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -123,7 +125,7 @@ button8.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button8.setEnabled(false);
             char mark = game.getMark();
-            button8.setText(mark + "");
+            setMark(button8,mark,  xIcon,  oIcon);
             game.placeMark(1,2);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -134,7 +136,7 @@ button11.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button11.setEnabled(false);
             char mark = game.getMark();
-            button11.setText(mark + "");
+            setMark(button11,mark,  xIcon,  oIcon);
             game.placeMark(2,0);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -145,7 +147,7 @@ button12.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button12.setEnabled(false);
             char mark = game.getMark();
-            button12.setText(mark + "");
+            setMark(button12,mark,  xIcon,  oIcon);
             game.placeMark(2,1);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -156,7 +158,7 @@ button13.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
             button13.setEnabled(false);
             char mark = game.getMark();
-            button13.setText(mark + "");
+            setMark(button13,mark,  xIcon,  oIcon);
             game.placeMark(2,2);
             if (game.checkForWin() == true){
                 reset(label5, label10, button1, button2, button3 , button6,  button7,  button8, button11, button12,   button13);
@@ -186,7 +188,14 @@ add(button15); // Goes into row 2, column 3
 setVisible(true);
 }
 boolean currentPlayer = true;
-
+public void setMark(JButton button, char mark, Icon xIcon, Icon yIcon){
+    if (mark == 'x') {
+        button.setIcon(xIcon);
+    }
+    else {
+        button.setIcon(yIcon);
+    }
+}
 //JButton label5, JButton label10, JButton button1,JButton button2,JButton button3 ,JButton button6, JButton button7, JButton button8,JButton button11,JButton button12,  JButton button13
 public void reset(JLabel label5, JLabel label10, JButton button1,JButton button2,JButton button3 ,
         JButton button6, JButton button7, JButton button8,JButton button11,JButton button12,  JButton button13) {
@@ -199,15 +208,15 @@ public void reset(JLabel label5, JLabel label10, JButton button1,JButton button2
     label10.setText(game.getoScore() + "");
     
     // Clears the current marks on the buttons
-    button1.setText("");
-    button2.setText("");
-    button3.setText("");
-    button6.setText("");
-    button7.setText("");
-    button8.setText("");
-    button11.setText("");
-    button12.setText("");
-    button13.setText("");
+    button1.setIcon(null);
+    button2.setIcon(null);
+    button3.setIcon(null);
+    button6.setIcon(null);
+    button7.setIcon(null);
+    button8.setIcon(null);
+    button11.setIcon(null);
+    button12.setIcon(null);
+    button13.setIcon(null);
     
     // Makes all the buttons pressable again
     button1.setEnabled(true);
@@ -235,6 +244,8 @@ clicks on the Calculate button.
     }
 }
 }
+
+
 
 
 
